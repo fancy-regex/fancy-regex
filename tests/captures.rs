877a189985259f62c85970bb255a0f9b8936a12c
Robin Stocker <robin@nibor.org>
2019-08-21 21:51:06 +1000

    Add test for captures_from_pos

--- tests/captures.rs
+++ tests/captures.rs
@@ -1,6 +1,4 @@
-
-
-use fancy_regex::Captures;
+use fancy_regex::{Captures, Match, Result};
 
 mod common;
 
@@ -10,13 +8,26 @@ fn captures_after_lookbehind() {
         r"\s*(?<=[() ])(@\w+)(\([^)]*\))?\s*",
         " @another(foo bar)   ",
     );
-    assert_eq!(Some("@another"), captures.get(1).map(|m| m.as_str()));
-    assert_eq!(Some("(foo bar)"), captures.get(2).map(|m| m.as_str()));
+    assert_match(captures.get(1), "@another", 1, 9);
+    assert_match(captures.get(2), "(foo bar)", 9, 18);
+}
+
+#[test]
+fn captures_from_pos() {
+    let regex = common::regex(r"(\d+)(?!a)");
+    let captures = assert_captures(regex.captures_from_pos("1b 2a 3b", 3));
+
+    assert_match(captures.get(0), "3", 6, 7);
+    assert_match(captures.get(1), "3", 6, 7);
 }
 
 fn captures<'a>(re: &str, text: &'a str) -> Captures<'a> {
     let regex = common::regex(re);
     let result = regex.captures(text);
+    assert_captures(result)
+}
+
+fn assert_captures(result: Result<Option<Captures<'_>>>) -> Captures<'_> {
     assert!(
         result.is_ok(),
         "Expected captures to succeed, but was {:?}",
@@ -29,4 +40,12 @@ fn captures<'a>(re: &str, text: &'a str) -> Captures<'a> {
         captures
     );
     captures.unwrap()
+}
+
+fn assert_match(m: Option<Match<'_>>, expected_text: &str, start: usize, end: usize) {
+    assert!(m.is_some(), "Expected match, but was {:?}", m);
+    let m = m.unwrap();
+    assert_eq!(m.as_str(), expected_text);
+    assert_eq!(m.start(), start);
+    assert_eq!(m.end(), end);
 }
