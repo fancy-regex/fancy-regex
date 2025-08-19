a875595530f9105e75807735170fce82d4ec00ef
Keith Hall <keith-hall@users.noreply.github.com>
2025-08-19 05:50:42 +0300

    fix path to javascript file

--- playground/web/app.js
+++ playground/web/app.js
@@ -4,7 +4,7 @@ import init, {
     parse_regex,
     analyze_regex,
     is_match
-} from '../pkg/fancy_regex_playground.js';
+} from './pkg/fancy_regex_playground.js';
 
 class FancyRegexPlayground {
     constructor() {
